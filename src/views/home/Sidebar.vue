3af9e8bfae6bcb66f3f3e1971d9ad89ad0caf4de
阳 冯 <dusky_anubis@hotmail.com>
2017-09-06 16:48:24 +0800

    Sidebar

---
+++ src/views/home/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="sidebar">
+    <el-menu :default-active="onRoutes" class="el-menu-vertical-demo" theme="light" unique-opened="true" router="true">
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    onRoutes () {
+      return this.$route.path.replace('/', '')
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  .sidebar{
+    display: block;
+    position: absolute;
+    width: 250px;
+    left: 0;
+    top: 70px;
+    bottom: 0;
+    background: #20a0ff;
+  }
+</style>
